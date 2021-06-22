a31b1c891cba3ca50da66fa377fe9c46e5224431
elbincenisev <elbincenisev@outlook.com>
2021-06-22 19:29:29 -0400

    Added event listener to start button

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let startButton = document.querySelector('#startBtn');  // This button starts the quiz
+
+function startQuiz() {
+    // Hide start section
+    // Show questions section
+    // Start the score timer
+    
+}
+
+startButton.addEventListener("click", function(event) {
+    startQuiz();
+});
